538a992109a465b439e12b1f8554f18f212204c5
Miri424 <mirmohsum3@gmail.com>
2025-01-05 20:45:58 +0400

    H-work || Snobella Web || Added

--- 19-SnobellaWebsite/assets/js/helper.js
+++ 19-SnobellaWebsite/assets/js/helper.js
@@ -3,16 +3,16 @@ export { setLocalStorage, getLocalStorage };
 function getLocalStorage(key) {
     try {
         const data = localStorage.getItem(key);
-        return data ? JSON.parse(data) : {};  // Return an empty object if no data exists
+        return data ? JSON.parse(data) : {}; 
     } catch (error) {
         console.error(`Error reading ${key} from localStorage`, error);
-        return {};  // Return empty object on error
+        return {};  
     }
 }
 
 function setLocalStorage(key, value) {
     try {
-        localStorage.setItem(key, JSON.stringify(value));  // Ensure value is properly stringified
+        localStorage.setItem(key, JSON.stringify(value)); 
     } catch (error) {
         console.error(`Error writing ${key} to localStorage`, error);
     }
